84d514c8a88b780b9268175843313d434a76f2e4
Gerhard R <gerd.r.devel@googlemail.com>
2012-06-11 11:45:39 +0200

    fix ops.c generation

--- gen-ops.c.pl
+++ gen-ops.c.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 sub m0_ops {
-	open my $fh, '<', $ENV{M0_OPS};
+	open my $fh, '<', $ENV{M0_OPS_FILE};
 	while(<$fh>) {
 		next unless /^\w/;
 		chomp;
--- src-ops.c
+++ src-ops.c
@@ -9,9 +9,9 @@ m0_opfunc *const M0_OP_FUNCS[M0_OPCOUNT_] = {
 
 bool m0_ops_run(m0_interp *interp, m0_callframe *cf)
 {
-	assert((*interp)[M0_IPD_OP_FUNCS].as_cptr == M0_OP_FUNCS);
+	assert(m0_interp_op_funcs(interp) == M0_OP_FUNCS);
 
-	for(;; cf = (*cf)[M0_REG_CF])
+	for(;; cf = m0_reg_cf(cf))
 	{
 		const m0_chunk *chunk = m0_interp_chunks(interp) + m0_reg_chunk(cf);
 		const m0_op *ops = chunk->bytecode_segment->blocks;
