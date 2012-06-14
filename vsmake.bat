83a822af8fca31bafe5773dbf2cadc499da3f2a3
Gerhard R <gerd.r.devel@googlemail.com>
2012-06-15 00:55:20 +0200

    update README and turn off cygwin warnings

--- vsmake.bat
+++ vsmake.bat
@@ -1,1 +1,5 @@
-@make SHELL=cmd .SHELLFLAGS=/c SEPARATOR=\ CC=cl I_FLAG=/I OBJ_FLAG=/Fo EXE_FLAG=/Fe C_FLAG=/c CFLAGS=/Tp RM=rm-f EXESUFFIX=.exe OBJSUFFIX=.obj %*
+@echo off
+setlocal
+set CYGWIN=nodosfilewarnings
+make SHELL=cmd .SHELLFLAGS=/c SEPARATOR=\ CC=cl I_FLAG=/I OBJ_FLAG=/Fo EXE_FLAG=/Fe C_FLAG=/c CFLAGS=/Tp RM=rm-f EXESUFFIX=.exe OBJSUFFIX=.obj %*
+endlocal
